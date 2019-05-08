45ab9f7c2176a0f1a492c21b3cb58244fac7abab
swq <1716122210@qq.com>
2019-05-08 10:32:25 +0800

    宋文倩的提交

---
+++ src/main/java/com/jk/bean/Postbean.java
@@ -0,0 +1,13 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Postbean implements Serializable {
+        private String pid;//角色id
+        private String pname;//角色名称
+
+
+}
